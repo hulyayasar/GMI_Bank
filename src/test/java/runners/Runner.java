3dafd33352a150022346a4566cd30209feb95933
ocakdans <selim.ocakdan@gmail.com>
2021-02-17 20:35:33 +0300

    viraBismillah2

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,21 @@
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/default-cucumber-reports",
+                "json:target/json-report/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        features = "src/test/resources\u2028/features",
+
+        glue = "stepdefinitions",
+        tags = "@feature1",
+        dryRun = false
+)
+public class Runner {
+
+}
