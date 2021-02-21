d1fcecbbf4d78bed1de3e135c2eca40388409e83
ozcankilic <o.kilic4426@gmail.com>
2021-02-21 12:26:05 -0800

    first commit from ozcan

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -10,10 +10,10 @@ import org.junit.runner.RunWith;
                 "json:target/json-report/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"
         },
-        features = "src/test/resources\u2028/features",
+        features = "src/test/resources/features",
 
         glue = "stepdefinitions",
-        tags = "@us_012",
+        tags = "@login",
         dryRun = false
 )
 public class Runner {
