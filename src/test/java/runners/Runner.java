52ecf335648a63b2aaf0d801bb000617ebfefa3f
Kubra <kacars2010@gmail.com>
2021-02-21 21:16:55 +0300

    US01

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources\u2028/features",
 
         glue = "stepdefinitions",
-        tags = "@feature1",
+        tags = "@register",
         dryRun = false
 )
 public class Runner {
