dd432cb49950aeb6ba3b98ff668bc507ee06926c
Sophia Byb <sophiabyb@gmail.com>
2021-02-24 14:22:35 -0500

    captain begins navigation

--- src/test/java/pages/CreateAccountPage.java
+++ src/test/java/pages/CreateAccountPage.java
@@ -1,72 +1,39 @@
 package pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
 import java.util.List;
-
 public class CreateAccountPage {
     public CreateAccountPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-
-    @FindBy(xpath = "//a[@class='d-flex align-items-center dropdown-toggle nav-link']")
-    public WebElement signInDropList;
-
-    @FindBy(linkText = "Sign in")
-    public WebElement signIn;
-
-    @FindBy(name = "username")
-    public WebElement username;
-
-    @FindBy(name = "password")
-    public WebElement password;
-
-    @FindBy(xpath = "//button[@type='submit']")
-    public WebElement signInButton;
-
     @FindBy(linkText = "My Operations")
     public WebElement myOperations;
-
     @FindBy(linkText = "Manage Accounts")
     public WebElement manageAccounts;
-
     @FindBy(linkText = "Create a new Account")
     public WebElement createANewAccount;
-
     @FindBy(id = "tp-account-description")
     public WebElement description;
-
     @FindBy(className = "invalid-feedback")
     public WebElement descRequired;
-
     @FindBy(className = "invalid-feedback")
     public WebElement balRequired;
-
     @FindBy(name = "balance")
     public WebElement balance;
-
     @FindBy(id = "tp-account-accountType")
     public WebElement accountType;
-
     @FindBy(id = "tp-account-accountStatusType")
     public WebElement accountStatusType;
-
     @FindBy(id = "tp-account-createDate")
     public WebElement createDate;
-
     @FindBy(id = "tp-account-closedDate")
     public WebElement closedDate;
-
     @FindBy(id = "tp-account-employee")
     public WebElement accountEmployee;
-
     @FindBy(id = "save-entity")
     public WebElement saveButton;
-
     @FindBy(xpath = "//tbody//tr[*]//td[2]")
     public List<WebElement> accountList;
-
 }
