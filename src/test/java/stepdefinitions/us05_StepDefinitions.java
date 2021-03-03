d99da9527db0578fa442a8fd57624abc94471549
hulyayasar <73432891+hulyayasar@users.noreply.github.com>
2021-03-03 15:45:40 -0500

    beforeDemo

--- src/test/java/stepdefinitions/us05_StepDefinitions.java
+++ src/test/java/stepdefinitions/us05_StepDefinitions.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import pages.LogInPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 import java.util.concurrent.TimeUnit;
 
@@ -35,25 +36,25 @@ public class us05_StepDefinitions {
 
     @When("user enters the username {string}")
     public void user_enters_the_username(String string) {
-
+        ReusableMethods.waitForVisibility(loginPage.userName,5);
         loginPage.userName.sendKeys(string);
     }
 
     @When("user enters the password {string}")
     public void user_enters_the_password(String string) {
-
+        ReusableMethods.waitForVisibility(loginPage.password,5);
         loginPage.password.sendKeys(string);
     }
 
     @When("click on the submit button")
     public void click_on_the_submit_button() {
-
+        ReusableMethods.waitForVisibility(loginPage.SinginButton,5);
       loginPage.SinginButton.click();
     }
 
     @Then("verify the error message contains {string}")
     public void verify_the_error_message_contains(String string) throws InterruptedException {
-        Thread.sleep(15);
+        //Thread.sleep(15);
         Driver.getDriver().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         waitForVisibility(loginPage.failedToSignIn,10);
         String message = loginPage.failedToSignIn.getText();
@@ -63,7 +64,7 @@ public class us05_StepDefinitions {
 
     @Then("verify the error message empty contains {string}")
     public void verifyTheErrorMessageEmptyContains(String string) throws InterruptedException {
-        Thread.sleep(10);
+        //Thread.sleep(10);
         Driver.getDriver().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         waitForVisibility(loginPage.emptyError,10);
         String message = loginPage.emptyError.getText();
@@ -73,7 +74,7 @@ public class us05_StepDefinitions {
     }
     @Then("verify the error message empty contains second {string}")
     public void verifyTheErrorMessageEmptyContainsSecond(String string) throws InterruptedException {
-        Thread.sleep(10);
+        //Thread.sleep(10);
         Driver.getDriver().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         waitForVisibility(loginPage.emptyErrorSecond,10);
         String message = loginPage.emptyErrorSecond.getText();
@@ -87,6 +88,7 @@ public class us05_StepDefinitions {
     }
     @Then("verify the resetPassword title is {string}")
     public void verify_the_resetPassword_title_is(String string) {
+        waitForVisibility(loginPage.resetTitle,5);
         String resetTittle = loginPage.resetTitle.getText();
         System.out.println(resetTittle);
         Assert.assertEquals(resetTittle,string);
@@ -99,6 +101,7 @@ public class us05_StepDefinitions {
     }
     @Then("verify the RegisterANewAccount title is {string}")
     public void verify_the_RegisterANewAccount_title_is(String string) {
+        waitForVisibility(loginPage.registrationTitle,5);
         String registrationTitle = loginPage.registrationTitle.getText();
         System.out.println(registrationTitle);
         Assert.assertEquals(registrationTitle,string);
